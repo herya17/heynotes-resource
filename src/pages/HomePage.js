100296186051723fd32efa92088ad0bd28d47cc0
Heriyanto <67692606+herya17@users.noreply.github.com>
2023-09-10 00:37:27 +0700

    Add files via upload

---
+++ src/pages/HomePage.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Loader from '../components/Loader';
+import NoteList from '../components/NoteList';
+import EmptyMessage from '../components/EmptyMessage';
+import LocaleContext from '../contexts/LocaleContext';
+
+function HomePage({ isLoading, filteredNotes }) {
+  const { localeContextValue } = React.useContext(LocaleContext);
+  const { locale } = localeContextValue;
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  return (
+    <>
+      {
+        filteredNotes.length > 0
+          ? <NoteList notes={filteredNotes} />
+          : <EmptyMessage 
+              message={locale === 'id' 
+                ? 'Catatan aktif kosong, tidak dapat ditemukan!' 
+                : 'Empty active note, could not be found!'} />
+      }
+    </>
+  );
+}
+
+HomePage.propTypes = {
+  isLoading: PropTypes.bool.isRequired,
+  filteredNotes: PropTypes.arrayOf(PropTypes.object),
+}
+
+export default HomePage;
